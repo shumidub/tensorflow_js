9d9d7422d8f365f3c0266401e22e73c7ff7a087a
Artem <artem.shumidub@gmail.com>
2019-10-15 08:17:45 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const a = [[1,2], [3,4]];
+
+// Создаем rank-2 тензор (матрица/массив)
+const b = tf.tensor([[1,2], [3,4]]);
+console.log('shape:', b.shape);
+b.print()
+
+// Вывод данных
+const g = tf.tensor([[1,2], [3,4]]);
+g.data().then((raw) => {
+  console.log('async raw value of g:', raw);
+});
+console.log('raw value of g:', g.dataSync());
+console.log('raw multidimensional value of g:', g.arraySync());
